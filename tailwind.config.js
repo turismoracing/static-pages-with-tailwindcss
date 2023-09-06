568a412ae43368713846760f162956cb15bb3d94
turismoracing <115661234+turismoracing@users.noreply.github.com>
2023-09-06 16:11:42 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./index.html",
+        "./standards.html",
+        "./contact.html",
+        "./delivery.html",
+        "./extended-warranty.html",
+        "./warranty.html",
+        "./zip.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
